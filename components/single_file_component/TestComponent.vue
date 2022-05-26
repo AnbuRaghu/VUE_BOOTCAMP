f07d75186fc550c4b5ed4fe5ed85a377ec067b4f
Anbalagan Amirthalingam <anbamirt@publicisgroupe.net>
2022-05-26 18:30:05 +0200

    initial commit

---
+++ components/single_file_component/TestComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Hello from Component {{ test }}
+    </div>
+</template>
+
+<script>
+    // here we have to export the module to import
+    module.exports = {
+        data: function () {
+            return {
+                test: 'test hello'
+            }
+        }
+    }
+</script>
+
+<style></style>
